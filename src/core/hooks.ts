94a0a98f756009851baf24d4b8ff90309c79eb08
fedealdosivi <fedepalo13@gmail.com>
2023-10-10 10:03:43 +0100

    added screenshots

--- src/core/hooks.ts
+++ src/core/hooks.ts
@@ -1,19 +1,35 @@
-import { After, AfterAll, Before } from "@cucumber/cucumber";
-import { chromium } from "@playwright/test";
+import { After, AfterAll, AfterStep, Before, Status } from "@cucumber/cucumber";
+import { Browser, chromium } from "@playwright/test";
 import { finalizeCoverage, saveV8Coverage } from "./coverageHelper";
 import { pageFixture } from "./pageFixture";
 
+let browser : Browser;
+
 Before(async () => {
-    const browser = await chromium.launch({ headless: false });
+    browser = await chromium.launch({ headless: false });
     const context = await browser.newContext();
     const page = await context.newPage();
     pageFixture.page = page;
     await pageFixture.page.coverage.startJSCoverage();
 });
 
+// After(async function(scenario) { // Using function instead of an arrow function
+//     const img = await pageFixture.page.screenshot({path: `./test-results/screenshots/${scenario.pickle.name}`, type:"png"});
+//     await this.attach(img, "image/png");
+// });
+
+AfterStep(async function({pickle, result}) { 
+    if(result.status == Status.FAILED){
+        const img = await pageFixture.page.screenshot({path: `./test-results/screenshots/${pickle.name}`, type:"png"});
+        await this.attach(img, "image/png");
+    }
+});
+    
+
 After(async () => {
     await saveV8Coverage(pageFixture.page);
     await pageFixture.page.close();
+    await browser.close();
 });
 
 AfterAll(async () => {
